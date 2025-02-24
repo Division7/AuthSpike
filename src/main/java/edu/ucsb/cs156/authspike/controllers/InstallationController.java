0287f78bf8330b23b50431c93593122c178b4177
Daniel Jensen <djensen2@outlook.com>
2025-02-23 22:29:58 -0800

    factored out JWT to a service

--- src/main/java/edu/ucsb/cs156/authspike/controllers/InstallationController.java
+++ src/main/java/edu/ucsb/cs156/authspike/controllers/InstallationController.java
@@ -4,11 +4,8 @@ import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.JsonNode;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import edu.ucsb.cs156.authspike.entities.Installation;
-import edu.ucsb.cs156.authspike.services.PrivateKeyService;
-import io.jsonwebtoken.Jwt;
+import edu.ucsb.cs156.authspike.services.JwtService;
 import io.jsonwebtoken.Jwts;
-import io.jsonwebtoken.SignatureAlgorithm;
-import io.jsonwebtoken.security.Keys;
 import org.springframework.boot.web.client.RestTemplateBuilder;
 import org.springframework.http.HttpEntity;
 import org.springframework.http.HttpHeaders;
@@ -20,28 +17,24 @@ import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.client.RestTemplate;
 
-import java.security.Key;
 import java.security.NoSuchAlgorithmException;
-import java.security.PrivateKey;
-import java.security.interfaces.RSAPrivateKey;
 import java.security.spec.InvalidKeySpecException;
 import java.time.Instant;
 import java.time.temporal.ChronoUnit;
-import java.time.temporal.TemporalUnit;
 import java.util.Date;
 
 
 @RestController
 @RequestMapping("/api/installations")
 public class InstallationController {
-    private final PrivateKeyService privateKeyService;
+    private final JwtService jwtService;
 
     private final RestTemplate restTemplate;
 
     private ObjectMapper objectMapper;
 
-    public InstallationController(PrivateKeyService privateKeyService, RestTemplateBuilder restTemplateBuilder,  ObjectMapper objectMapper) {
-        this.privateKeyService = privateKeyService;
+    public InstallationController(JwtService jwtService, RestTemplateBuilder restTemplateBuilder, ObjectMapper objectMapper) {
+        this.jwtService = jwtService;
         restTemplate = restTemplateBuilder.build();
         this.objectMapper = objectMapper;
     }
@@ -49,45 +42,29 @@ public class InstallationController {
     @GetMapping("installation")
     public Object addInstallation(@RequestParam String installation_id,  @RequestParam String setup_action) throws NoSuchAlgorithmException, InvalidKeySpecException {
         Installation installation = new Installation();
-        String token = Jwts.builder()
-                .issuedAt(Date.from(Instant.now()))
-                .expiration(Date.from(Instant.now().plus(5, ChronoUnit.MINUTES)))
-                .issuer(privateKeyService.getClientId())
-                .signWith(privateKeyService.getPrivateKey())
-                .compact();
-        return token;
-    }
-
-    @GetMapping("privateKey")
-    public String getToken() throws NoSuchAlgorithmException, InvalidKeySpecException, JsonProcessingException {
-        String token = Jwts.builder()
-                .issuedAt(Date.from(Instant.now().minus(30, ChronoUnit.SECONDS)))
-                .expiration(Date.from(Instant.now().plus(5, ChronoUnit.MINUTES)))
-                .issuer(privateKeyService.getClientId())
-                .signWith(privateKeyService.getPrivateKey(), SignatureAlgorithm.RS256)
-                .compact();
-        String ENDPOINT = "https://api.github.com/app/installations/61554210/access_tokens";
+        String token = jwtService.getJwt();
+        String ENDPOINT = "https://api.github.com/app/installations/"+installation_id;
 
         HttpHeaders headers = new HttpHeaders();
         headers.add("Authorization", "Bearer " + token);
         headers.add("Accept", "application/vnd.github+json");
         headers.add("X-GitHub-Api-Version", "2022-11-28");
         HttpEntity<String> entity = new HttpEntity<>(headers);
-        ResponseEntity<String> response = restTemplate.exchange(ENDPOINT, HttpMethod.POST,  entity, String.class);
+        ResponseEntity<String> response = restTemplate.exchange(ENDPOINT, HttpMethod.GET,  entity, String.class);
+        return response.getBody();
+    }
 
-        System.out.println("response: " + response.getBody());
-        JsonNode responseJson = objectMapper.readTree(response.getBody());
-        String installationToken = responseJson.get("token").asText();
+    @GetMapping("privateKey")
+    public String getToken() throws NoSuchAlgorithmException, InvalidKeySpecException, JsonProcessingException {
 
-        System.out.println("token: " + installationToken);
+        String token = jwtService.getInstallationToken("61554210");
 
         HttpHeaders requestHeaders = new HttpHeaders();
-        requestHeaders.add("Authorization", "Bearer " + installationToken);
+        requestHeaders.add("Authorization", "Bearer " + token);
         requestHeaders.add("Accept", "application/vnd.github+json");
         requestHeaders.add("X-GitHub-Api-Version", "2022-11-28");
-        HttpEntity<String> newEntity = new HttpEntity<>(requestHeaders.toString());
+        HttpEntity<String> newEntity = new HttpEntity<>(requestHeaders);
 
-        System.out.println("newEntity: " + newEntity.getBody());
         String NEWENDPOINT = "https://api.github.com/users/Division7/repos";
         ResponseEntity<String> newResponse = restTemplate.exchange(NEWENDPOINT, HttpMethod.GET,  newEntity, String.class);
 
