e942102fe1ec4db37b9d245295d913010e486080
Daniel Jensen <djensen2@outlook.com>
2025-03-11 13:32:10 -0700

    dj - added correct url prefix

--- startup.sh
+++ startup.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 export JDBC_DATABASE_PASSWORD=$(echo "$DATABASE_URL" | cut --delimiter=: -f3 | cut --delimiter=\@ -f1)
 
-export JDBC_DATABASE_URL=$(echo "$DATABASE_URL" | cut --delimiter=\@ -f2)
+export JDBC_DATABASE_URL=jdbc:postgresql://$(echo "$DATABASE_URL" | cut --delimiter=\@ -f2)
 
 export JDBC_DATABASE_USERNAME=postgres
 
