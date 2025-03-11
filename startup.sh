ca83359667498a9756066071c10be6c3b7b0d4f6
Daniel Jensen <djensen2@outlook.com>
2025-03-11 13:23:40 -0700

    dj - used ENV instead of export this time

--- startup.sh
+++ startup.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
-EXPORT JDBC_DATABASE_PASSWORD=$(echo "$DATABASE_URL" | cut --delimiter=: -f3 | cut --delimiter=\@ -f1)
+ENV JDBC_DATABASE_PASSWORD=$(echo "$DATABASE_URL" | cut --delimiter=: -f3 | cut --delimiter=\@ -f1)
 
-EXPORT JDBC_DATABASE_URL=$(echo "$DATABASE_URL" | cut --delimiter=\@ -f2)
+ENV JDBC_DATABASE_URL=$(echo "$DATABASE_URL" | cut --delimiter=\@ -f2)
 
-EXPORT JDBC_DATABASE_USERNAME=postgres
+ENV JDBC_DATABASE_USERNAME=postgres
 
 java -jar $1
