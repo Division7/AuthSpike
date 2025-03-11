8571abffe670f9f1b49e628e4b7ea3a261cc52fd
Daniel Jensen <djensen2@outlook.com>
2025-03-11 13:25:47 -0700

    dj - back to export. Lowercase this time

--- startup.sh
+++ startup.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
-ENV JDBC_DATABASE_PASSWORD=$(echo "$DATABASE_URL" | cut --delimiter=: -f3 | cut --delimiter=\@ -f1)
+export JDBC_DATABASE_PASSWORD=$(echo "$DATABASE_URL" | cut --delimiter=: -f3 | cut --delimiter=\@ -f1)
 
-ENV JDBC_DATABASE_URL=$(echo "$DATABASE_URL" | cut --delimiter=\@ -f2)
+export JDBC_DATABASE_URL=$(echo "$DATABASE_URL" | cut --delimiter=\@ -f2)
 
-ENV JDBC_DATABASE_USERNAME=postgres
+export JDBC_DATABASE_USERNAME=postgres
 
 java -jar $1
